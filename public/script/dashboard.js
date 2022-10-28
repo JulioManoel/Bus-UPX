e58bf3b360ea85ba6c501ca1d8703f29d93bf99e
JulioManoel <juliobonow@gmail.com>
2022-10-28 16:53:37 -0300

    Atualização Dashboars

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -1,3 +1,4 @@
+var dadosOi
 function data(json){
     var dataAtual = new Date()
     var dia = dataAtual.getDate()
@@ -5,37 +6,54 @@ function data(json){
     var ano = dataAtual.getFullYear()
     var dadosDia = json[ano][mes][dia];
     var passageiros = 0;
-    var dinheiro = 0;
+    var contador = 0;
     var onibus = Object.keys(dadosDia).length;
     for(var i=0; i<Object.keys(dadosDia).length; i++){
         passageiros += Object.values(dadosDia)[i].Passageiros
-        dinheiro += Object.values(dadosDia)[i].Contador
+        contador += Object.values(dadosDia)[i].Contador
     }
+    var valorPassagem = json.Valor
+    var dinheiro = contador*valorPassagem
     document.querySelector("#passageiros").innerText = passageiros
     document.querySelector("#onibus").innerText = onibus
-    document.querySelector("#money-value").innerText = (dinheiro*json.Valor).toFixed(2)
+    document.querySelector("#contador").innerText = contador
+    document.querySelector("#money-value").innerText = dinheiro.toFixed(2)
     tabela(dadosDia);
+    eventListener(dadosDia, valorPassagem);
 }
 
 function tabela(dadosDia){
     for(var i=0; i<Object.keys(dadosDia).length; i++){
         var div = document.createElement("div")
         var img = document.createElement("img")
-        var p = document.createElement("p")
+        var linha = document.createElement("p")
 
         div.classList.add("tabela")
-        /*div.onclick = (dadosDia, i) => {
-            var passageiros = Object.values(dadosDia)[i].Passageiros
-            var dinheiro = Object.values(dadosDia)[i].Contador
-            document.querySelector("#passageiros").innerText = passageiros
-            document.querySelector("#onibus").innerText = 1
-            document.querySelector("#money-value").innerText = (dinheiro*json.Valor).toFixed(2)
-        }*/
         img.src = "/public/icon/busIcon.svg"
-        p.innerText = Object.values(dadosDia)[i].Linha
+        linha.innerText = Object.values(dadosDia)[i].Linha
 
-        div.appendChild(img);
-        div.appendChild(p)
+        div.appendChild(img)
+        div.appendChild(linha)
         document.querySelector("#tabela").appendChild(div)
+    }
+}
+
+function eventListener(dadosDia, valorPassagem){
+    var linhas = document.querySelectorAll(".tabela");
+    for (var i = 0; i < linhas.length; i++) {
+        linhas[i].addEventListener("click", function (e) {
+            dadosOi = dadosDia
+            for(var j=0; j<Object.keys(dadosDia).length; j++){
+                if(Object.values(dadosDia)[j].Linha == this.innerText){
+                    var passageiros = Object.values(dadosDia)[j].Passageiros
+                    var contador = Object.values(dadosDia)[j].Contador
+                    var dinheiro = contador*valorPassagem
+                    document.querySelector("#passageiros").innerText = passageiros
+                    document.querySelector("#onibus").innerText = 1
+                    document.querySelector("#contador").innerText = contador
+                    document.querySelector("#money-value").innerText = dinheiro.toFixed(2)
+                }
+            }
+        })
     }
 }
